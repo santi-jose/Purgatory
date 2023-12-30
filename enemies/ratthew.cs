aaccc9abb7c9f8e24c9a3f2532a085206cb8c149
jlsantia21 <jlsantia21@gmail.com>
2023-12-30 00:02:46 -0800

    Adding ratthew enemy C# file.

---
+++ enemies/ratthew.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class ratthew : RigidBody2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    [Export]
+    public int Speed = 0;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
